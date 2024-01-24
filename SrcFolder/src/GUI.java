87ebc30c6e3db40f71d5a204fc748876f43d7127
Silas van der Waal <silasvanderwaal@gmail.com>
2024-01-24 21:16:35 +0100

    Created a windows with a text field and a scrollbar

--- SrcFolder/src/GUI.java
+++ SrcFolder/src/GUI.java
@@ -1,10 +1,15 @@
+package src;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class GUI {
 	
 	JFrame window;
 	JTextArea textArea;
+	JScrollPane scrollPanel;
 
 	public static void main(String[] args) {
 		
@@ -29,6 +34,10 @@ public class GUI {
 	public void createTextArea() {
 		textArea = new JTextArea();
 		window.add(textArea);
+		
+		scrollPanel = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPanel.setBorder(BorderFactory.createEmptyBorder());
+		window.add(scrollPanel);
 	}
 
 }
