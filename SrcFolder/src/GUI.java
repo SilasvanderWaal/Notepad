8aed11948a6bf3b2bde05a82f9c5293cd3628c74
Silas van der Waal <silasvanderwaal@gmail.com>
2024-01-24 22:46:39 +0100

    Menu bar and menu items added

--- SrcFolder/src/GUI.java
+++ SrcFolder/src/GUI.java
@@ -2,6 +2,9 @@ package src;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -10,6 +13,9 @@ public class GUI {
 	JFrame window;
 	JTextArea textArea;
 	JScrollPane scrollPanel;
+	JMenuBar menuBar;
+	JMenu menuFile, menuEdit, menuFormat, menuColor;
+	JMenuItem iNew, iOpen, iSave, iSaveAs, iExit, iUndo, iRedo;
 
 	public static void main(String[] args) {
 		
@@ -20,6 +26,8 @@ public class GUI {
 	public GUI() {
 		createWindow();
 		createTextArea();
+		createMenuBar();
+		createFileMenu();
 		
 		window.setVisible(true);
 	}
@@ -38,6 +46,46 @@ public class GUI {
 		scrollPanel = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		scrollPanel.setBorder(BorderFactory.createEmptyBorder());
 		window.add(scrollPanel);
+	}
+	
+	public void createMenuBar() {
+		menuBar = new JMenuBar();
+		window.setJMenuBar(menuBar);
+		
+		menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		menuEdit = new JMenu("Edit");
+		menuBar.add(menuEdit);
+		
+		menuFormat = new JMenu("Format");
+		menuBar.add(menuFormat);
+		
+		menuColor = new JMenu("Color");
+		menuBar.add(menuColor);
+	}
+	
+	public void createFileMenu() {
+		iNew = new JMenuItem("New");
+		menuFile.add(iNew);
+		
+		iOpen = new JMenuItem("Open");
+		menuFile.add(iOpen);
+		
+		iSave = new JMenuItem("Save");
+		menuFile.add(iSave);
+		
+		iSaveAs = new JMenuItem("Save as");
+		menuFile.add(iSaveAs);
+		
+		iExit = new JMenuItem("Exit");
+		menuFile.add(iExit);
+		
+		iUndo = new JMenuItem("Undo");
+		menuEdit.add(iUndo);
+		
+		iRedo = new JMenuItem("Redo");
+		menuEdit.add(iRedo);
 	}
 
 }
