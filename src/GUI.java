fbc348f413e3320351a674edf985a557c305a3ad
Silas van der Waal <silasvanderwaal@gmail.com>
2024-01-24 20:44:28 +0100

    Sync to github

---
+++ src/GUI.java
@@ -0,0 +1,34 @@
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class GUI {
+	
+	JFrame window;
+	JTextArea textArea;
+
+	public static void main(String[] args) {
+		
+		new GUI();
+		
+	}
+	
+	public GUI() {
+		createWindow();
+		createTextArea();
+		
+		window.setVisible(true);
+	}
+	
+	public void createWindow() {
+		 
+		window = new JFrame("Notepad");
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public void createTextArea() {
+		textArea = new JTextArea();
+		window.add(textArea);
+	}
+
+}
